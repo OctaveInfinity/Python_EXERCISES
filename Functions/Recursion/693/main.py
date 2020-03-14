52612611525c17194ef69dfcab3c2e02171eabad
OctaveInfinity <e.makoviak@gmail.com>
2020-03-14 22:04:48 +0200

    benchmark

--- Functions/Recursion/693/main.py
+++ Functions/Recursion/693/main.py
@@ -1,10 +1,11 @@
 from myfunction import add_function
 
+# for crash test
+MAX_VALUE = 10*100 # working good !
 
-MAX_VALUE = 10**3
 
-
-add_function_test_values = {
+# test_values
+test_values = {
     (+0, +7): +7,
     (+7, +0): +7,
     (+2, +2): +4,
@@ -16,16 +17,18 @@ add_function_test_values = {
     (-5, +2): -3,
     (-2, +5): +3,
     (+5, -2): +3,
-    (2, -5): -3,
-    (2, MAX_VALUE): (MAX_VALUE + 2),
+    (+2, -5): -3,
+    (+2, MAX_VALUE): (MAX_VALUE + 2),
+    (-2, -MAX_VALUE): (-MAX_VALUE - 2),
 }
 
-for arguments, check_value in add_function_test_values.items():
+# run benchmark test
+for arguments, check_value in test_values.items():
 
+    # add_function
     result = add_function(*arguments)
-
     if result == check_value:
         print(f"OK! {arguments} = {result}.\n")
-
     else:
         print(f"WRONG! {arguments} = {result}. Must be {check_value}!\n")
+
