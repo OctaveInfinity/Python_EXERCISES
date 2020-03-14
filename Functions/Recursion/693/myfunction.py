2b4fd31dad5ee10072ad0526579d21d5677c32a6
OctaveInfinity <e.makoviak@gmail.com>
2020-03-14 18:35:49 +0200

    change condition for end of recursion from 1 to 0

--- Functions/Recursion/693/myfunction.py
+++ Functions/Recursion/693/myfunction.py
@@ -1,84 +1,47 @@
 
-def recursion(a, b):
-    
-    def template(a, b, e, s):
-        # print(a, b)
-        if abs(a) >= abs(b):
-            if  b == 1*s:
-                # print()
-                return a+1*s
-            else:
-                return template(a+1*s*e, b-1*s*e, e, s)
+def add_function(a, b):
+
+    def add_recursion(x, y):
+
+        # chek only second operand
+        if y > 0:
+            return add_recursion(x, y-1) + 1
+        elif y < 0:
+            return add_recursion(x, y+1) - 1
         else:
-            a, b = b, a
-            # print("Swapp")
-            return template(a, b, e, s)
+            # end condition: y == 0
+            return x
 
-    # (+, +)
-    if a > 0 and b > 0:
-
-        return template(a, b, 1, 1) # ok
-
-    # (-, -)
-    if a < 0 and b < 0:
-
-        return template(a, b, +1, -1) # ok
-
-    # (-, +)
-    if a < 0 and b > 0:
-
-        if abs(a) > abs(b):
-            return template(a, b, 1, 1) # ok
-
-        if abs(a) < abs(b):
-            return template(a, b, -1, 1) # ok
-
-    # (+, -)
-    if a > 0 and b < 0:
-
-        if abs(a) > abs(b):
-            return template(a, b, +1, -1) # ok
-
-        if abs(a) < abs(b):
-            return template(a, b, -1, -1) # ok
-
-
-    # (0, 7) = 7
-    if a == 0:
-        return b
-
-    # (7, 0) = 7
-    if b == 0:
-        return a
+    # optimization part
+    if abs(a) >= abs(b):
+        return add_recursion(a, b)
+    else:
+        a, b = b, a
+        return add_recursion(a, b)
 
 
 
-# x = int(input("x: "))
-# y = int(input("y: "))        
-
-# res = recursion(x, y)
-# print(res)
-
-
-test_values = {
-    (+0, +3): +3,
-    (+3, +0): +3,
+add_function_test_values = {
+    (+0, +7): +7,
+    (+7, +0): +7,
+    (+2, +2): +4,
     (+3, +2): +5,
     (+2, +3): +5,
-    (+2, +2): +4,
     (-3, -2): -5,
     (-2, -3): -5,
     (-2, -2): -4,
     (-5, +2): -3,
     (-2, +5): +3,
     (+5, -2): +3,
-    (+2, -5): -3,
+    (2, -5): -3,
 }
 
-for key, value in test_values.items():
+for arguments, check_value in add_function_test_values.items():
 
-    if recursion(*key) == value:
-        print(f'YES resursion{key} == {recursion(*key)}.')
+    result = add_function(*arguments)
+
+    if result == check_value:
+        print(f"OK! {arguments} = {result}.\n")
 
     else:
-        print(f'! Not ! resursion{key} == {recursion(*key)}. Must be {value}')
+        print(f"WRONG! {arguments} = {result}. Must be {check_value}!\n")
