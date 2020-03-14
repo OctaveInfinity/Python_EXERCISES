6438d603f4be8d2c1a81ea8132781057ba6a4e56
OctaveInfinity <e.makoviak@gmail.com>
2020-03-14 18:26:09 +0200

    continue refactoring

--- Functions/Recursion/693/myfunction.py
+++ Functions/Recursion/693/myfunction.py
@@ -1,70 +1,47 @@
+
 def recursion(a, b):
     
+    def template(a, b, e, s):
+        # print(a, b)
+        if abs(a) >= abs(b):
+            if  b == 1*s:
+                # print()
+                return a+1*s
+            else:
+                return template(a+1*s*e, b-1*s*e, e, s)
+        else:
+            a, b = b, a
+            # print("Swapp")
+            return template(a, b, e, s)
+
     # (+, +)
     if a > 0 and b > 0:
 
-        # (3, 2) = 5
-        if abs(a) > abs(b): 
-            if  b == 1:
-                return a+1
-            else:
-                return recursion(a+1, b-1)
-
-        # (2, 3) = 5
-        if abs(a) <= abs(b):            
-            a, b = b, a
-            print("Swapp")
-            return recursion(a, b)
-
+        return template(a, b, 1, 1) # ok
 
     # (-, -)
     if a < 0 and b < 0:
 
-        # (-3, -2) = -5
-        if abs(a) >= abs(b): 
-            if  b == -1:
-                return a-1
-            else:
-                return recursion(a-1, b+1)
+        return template(a, b, +1, -1) # ok
 
-        # (-2, -3) = -5
-        if abs(a) < abs(b):
-            a, b = b, a
-            print("Swapp")
-            return recursion(a, b)
-  
     # (-, +)
     if a < 0 and b > 0:
 
-        # (-5, 2) = -3
         if abs(a) > abs(b):
-            if  b == 1:
-                return a+1
-            else:
-                return recursion(a+1, b-1)
-                
-        
-        # (-2, 5) = 3
+            return template(a, b, 1, 1) # ok
+
         if abs(a) < abs(b):
-            if  a == -1:
-                return b-1
-            else:
-                return recursion(a+1, b-1)
+            return template(a, b, -1, 1) # ok
 
     # (+, -)
     if a > 0 and b < 0:
 
-        # (5, -2) = 3
         if abs(a) > abs(b):
-            a, b = b, a
-            print("Swapp")
-            return recursion(a, b)
+            return template(a, b, +1, -1) # ok
 
-        # (2, -5) = -3
         if abs(a) < abs(b):
-            a, b = b, a
-            print("Swapp")
-            return recursion(a, b)
+            return template(a, b, -1, -1) # ok
+
 
     # (0, 7) = 7
     if a == 0:
@@ -82,3 +59,26 @@ def recursion(a, b):
 # res = recursion(x, y)
 # print(res)
 
+
+test_values = {
+    (+0, +3): +3,
+    (+3, +0): +3,
+    (+3, +2): +5,
+    (+2, +3): +5,
+    (+2, +2): +4,
+    (-3, -2): -5,
+    (-2, -3): -5,
+    (-2, -2): -4,
+    (-5, +2): -3,
+    (-2, +5): +3,
+    (+5, -2): +3,
+    (+2, -5): -3,
+}
+
+for key, value in test_values.items():
+
+    if recursion(*key) == value:
+        print(f'YES resursion{key} == {recursion(*key)}.')
+
+    else:
+        print(f'! Not ! resursion{key} == {recursion(*key)}. Must be {value}')
